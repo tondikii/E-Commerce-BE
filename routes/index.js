3247bf0ef1151aebc6058b22acf36f51abd87f85
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-02-16 14:54:44 +0700

    delete config netlify

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ const router = require("express").Router();
 const userRouter = require("./user");
 const productRouter = require("./product");
 const multer = require("../middlewares/multer")
-router.get("/", (req, res) => {
-  res.json({ message: 'Hello from the serverless function!' });
-})
 router.use("/users", userRouter);
 router.use("/products", multer, productRouter);
 

824da0139e145cc46363467fe8d0617ac3b7ccb0
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-02-16 14:16:44 +0700

    test deploy 1

---
+++ fuctions/api.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const port = process.env.PORT || 3001
+const cors = require("cors")
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+const routes = require("../routes")
+app.use("/.netlify/functions/api", routes)
+
+// Error Handler
+const errorHandler = require("./middlewares/errorHandler")
+app.use(errorHandler)
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+module.exports.handler = serverless(app)

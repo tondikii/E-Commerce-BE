8d89bc7810f941e65595d98f331da97eaf16302b
Tondiki Andika <tondikiandika@Tondikis-MacBook-Air.local>
2024-02-16 14:34:36 +0700

    test deploy sample app

--- fuctions/api.js
+++ fuctions/api.js
@@ -1,27 +1,38 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express = require('express')
-const serverless = require('serverless-http')
-const app = express()
-const port = process.env.PORT || 3001
-const cors = require("cors")
+// const express = require('express')
+// const serverless = require('serverless-http')
+// const app = express()
+// const port = process.env.PORT || 3001
+// const cors = require("cors")
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-// Routes
-const routes = require("../routes")
-app.use("/.netlify/functions/api", routes)
+// // Routes
+// const routes = require("../routes")
+// app.use("/.netlify/functions/api", routes)
 
-// Error Handler
-const errorHandler = require("../middlewares/errorHandler")
-app.use(errorHandler)
+// // Error Handler
+// const errorHandler = require("../middlewares/errorHandler")
+// app.use(errorHandler)
 
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
-module.exports.handler = serverless(app)
+// module.exports.handler = serverless(app)
+
+// functions/hello.js
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from Express serverless function!' });
+});
+
+module.exports.handler = serverless(app);
